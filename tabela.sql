15d378d11555d940a38f3e1a953b904b79949536
internet <internet@L05-0301>
2019-10-02 22:46:05 -0300

    tabela

---
+++ tabela.sql
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="1;url=http://localhost:8890/lab?token=37bdcc7726f2262d510024c0c8d338eb1d063c9ab158e08f" />
+    <title>Opening Jupyter Notebook</title>
+    <script>
+      setTimeout(function() {
+        window.location.href = "http://localhost:8890/lab?token=37bdcc7726f2262d510024c0c8d338eb1d063c9ab158e08f";
+      }, 1000);
+    </script>
+</head>
+<body>
+
+<p>
+    This page should redirect you to Jupyter Notebook. If it doesn't,
+    <a href="http://localhost:8890/lab?token=37bdcc7726f2262d510024c0c8d338eb1d063c9ab158e08f">click here to go to Jupyter</a>.
+</p>
+
+</body>
+</html>
