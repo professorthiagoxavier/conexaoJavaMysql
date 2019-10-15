8474a8ab24233b0d8d316efd37a4d92ee4e7efc1
professorthiagoxavier <54223083+professorthiagoxavier@users.noreply.github.com>
2019-10-15 19:59:16 -0300

    Update tabela.sql

--- tabela.sql
+++ tabela.sql
@@ -1,22 +1,9 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="1;url=http://localhost:8890/lab?token=37bdcc7726f2262d510024c0c8d338eb1d063c9ab158e08f" />
-    <title>Opening Jupyter Notebook</title>
-    <script>
-      setTimeout(function() {
-        window.location.href = "http://localhost:8890/lab?token=37bdcc7726f2262d510024c0c8d338eb1d063c9ab158e08f";
-      }, 1000);
-    </script>
-</head>
-<body>
-
-<p>
-    This page should redirect you to Jupyter Notebook. If it doesn't,
-    <a href="http://localhost:8890/lab?token=37bdcc7726f2262d510024c0c8d338eb1d063c9ab158e08f">click here to go to Jupyter</a>.
-</p>
-
-</body>
-</html>
+CREATE TABLE `carros` (
+  `idCarro` INT NOT NULL AUTO_INCREMENT,
+  `fabricante` VARCHAR(32) NULL,
+  `modelo` VARCHAR(45) NULL,
+  `anoFabricacao` INT NULL,
+  `cor` VARCHAR(16) NULL,
+  `km` DECIMAL(8,2) NULL,
+  `valor` DECIMAL(10,2) NULL,
+  PRIMARY KEY (`idCarro`));
